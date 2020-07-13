cd5b1b6cc4235ce564ab6c7c2044cfde9b9e4ef9
Joshua Schmitt <joshuaschmitt@Joshuas-MacBook-Pro.local>
2020-07-13 15:23:05 -0400

    modified routes

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Joshua'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Courses</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
