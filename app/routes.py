2614921e7b212102fdd89e9a3fe0af3dcc6f587c
Joshua Schmitt <joshuaschmitt@Joshuas-MacBook-Pro.local>
2020-07-14 16:28:24 -0400

    Added sign in page with form validation

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,23 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Joshua'}
-    return render_template('index.html', title = 'Home', user = user)
+    return render_template('index.html', user = user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    """
+    form.validate_on_submit() will return false if a GET request is sent and 
+    will continue onto the render_template statement. If a POST request is sent 
+    it will run the validators and may return true.
+    """
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',form = form )
